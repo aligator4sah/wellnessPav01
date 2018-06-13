28520f1c991220bdef90287cb0765e1f825d926e
Aligator_sah <saixiongh@outlook.com>
2018-06-13 17:25:10 -0400

    change left bar outlook

--- src/app/layout/left-nav/left-nav.component.ts
+++ src/app/layout/left-nav/left-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-left-nav',
@@ -6,10 +7,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-nav.component.css']
 })
 export class LeftNavComponent implements OnInit {
+  items: MenuItem[];
 
   constructor() { }
 
   ngOnInit() {
+    this.items = [
+      {
+        label: 'Patient', icon: 'fa fa-fw fa-check',
+        items: [
+          [
+            {
+              label: 'TV 1',
+              items: [{label: 'TV 1.1'}, {label: 'TV 1.2'}]
+            },
+            {
+              label: 'TV 2'
+            }
+          ],
+          [
+            {
+              label: 'TV 3'
+            },
+            {
+              label: 'TV 4'
+            }
+          ]
+        ]
+      },
+      {
+        label: 'Appointments', icon: 'fa fa-fw fa-calendar',
+        items: [
+          [
+            {
+              label: 'Sports 1',
+              items: [{label: 'Sports 1.1'}, {label: 'Sports 1.2'}]
+            },
+            {
+              label: 'Sports 2',
+              items: [{label: 'Sports 2.1'}, {label: 'Sports 2.2'}]
+            },
+
+          ],
+          [
+            {
+              label: 'Sports 3',
+              items: [{label: 'Sports 3.1'}, {label: 'Sports 3.2'}]
+            },
+            {
+              label: 'Sports 4',
+              items: [{label: 'Sports 4.1'}, {label: 'Sports 4.2'}]
+            }
+          ],
+          [
+            {
+              label: 'Sports 5',
+              items: [{label: 'Sports 5.1'}, {label: 'Sports 5.2'}]
+            },
+            {
+              label: 'Sports 6',
+              items: [{label: 'Sports 6.1'}, {label: 'Sports 6.2'}]
+            }
+          ]
+        ]
+      }
+    ];
   }
 
 }
--- src/app/layout/left-nav/left-nav.module.ts
+++ src/app/layout/left-nav/left-nav.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeftNavComponent } from './left-nav.component';
 import {ShareModule} from '../../share/share.module';
+import {MegaMenuModule} from 'primeng/primeng';
 
 @NgModule({
   imports: [
     CommonModule,
-    ShareModule
+    ShareModule,
+    MegaMenuModule,
   ],
   declarations: [LeftNavComponent],
   exports: [
     ShareModule,
-    LeftNavComponent
+    LeftNavComponent,
+    MegaMenuModule,
   ]
 })
 export class LeftNavModule { }
