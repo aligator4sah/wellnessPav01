a663f73f7114a60fe629e8ff0cbd03303a9a7722
Aligator_sah <saixiongh@outlook.com>
2018-06-15 17:33:52 -0400

    complete basic information input form

--- src/app/layout/dashboard/create-demographic/create-demographic.component.ts
+++ src/app/layout/dashboard/create-demographic/create-demographic.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateDemographicComponent implements OnInit {
 
+  value: Date;
+  selectedRace: string;
+  selectedGender: string;
+  genders = GENDER;
+  races = RACE;
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
+
+export const GENDER = [
+  {label: 'Gender', value: null},
+  {label: 'Male', value: 'Male'},
+  {label: 'Female', value: 'Female'}
+];
+
+export const RACE = [
+  {label: 'Race', value: null},
+  {label: 'Native American', value: 'Native American'},
+  {label: 'Asian or Pacific Island', value: ''},
+];
--- src/app/layout/dashboard/create-demographic/create-demographic.module.ts
+++ src/app/layout/dashboard/create-demographic/create-demographic.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { CreateDemographicComponent } from './create-demographic.component';
 import {ShareModule} from '../../../share/share.module';
 import {Router, RouterModule} from '@angular/router';
+import {CalendarModule, DropdownModule, InputTextModule, PanelMenuModule, RadioButton, RadioButtonModule} from 'primeng/primeng';
+import {PanelModule} from 'primeng/panel';
 
 @NgModule({
   imports: [
     CommonModule,
     ShareModule,
+    PanelModule,
+    InputTextModule,
+    CalendarModule,
+    RadioButtonModule,
+    DropdownModule,
     RouterModule.forChild([
       {path: '', component: CreateDemographicComponent}
     ])
@@ -16,6 +23,11 @@ import {Router, RouterModule} from '@angular/router';
   exports: [
     ShareModule,
     RouterModule,
+    PanelModule,
+    CalendarModule,
+    InputTextModule,
+    DropdownModule,
+    RadioButtonModule,
     CreateDemographicComponent
   ]
 })
