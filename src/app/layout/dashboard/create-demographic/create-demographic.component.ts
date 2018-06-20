2f5f749d7dede35648e49a4ebe9a4345caa93425
yunfan zhou <yunfanzhou@yunfans-MBP.fios-router.home>
2018-06-19 20:40:31 -0400

    demo form

--- src/app/layout/dashboard/create-demographic/create-demographic.component.ts
+++ src/app/layout/dashboard/create-demographic/create-demographic.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import {GENDER, RACE, STATES, LIVING, RESIDENT, MARRYSTATUS, EMPLOYMENTS, EMPLOYSTATUS} from './../../../model/mock';
 @Component({
   selector: 'app-create-demographic',
   templateUrl: './create-demographic.component.html',
@@ -30,29 +30,44 @@ export class CreateDemographicComponent implements OnInit {
   employStatus = EMPLOYMENTS;
   employTypes = EMPLOYSTATUS;
 
-  additionalForm: FormGroup;
+  createDemographicForm: FormGroup;
 
 
   constructor(
     private fb : FormBuilder
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.additionalForm = this.fb.group({
+    this.createDemographicForm = this.fb.group({
+      firstName: ['', Validators.required,],
+      middleName: [''],
+      lastName: ['', Validators.required],
+      birthday: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      email: ['', Validators.required, Validators.email],
+      gender: ['', Validators.required],
+      race: ['', Validators.required],
+      address1: ['', Validators.required],
+      address2: [''],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipcode: ['', [Validators.required, Validators.pattern('[0-9]{5}')]],
+      livingArranges: ['', Validators.required],
+      residentCondition: ['', Validators.required],
       married: ['', Validators.required],
       together:[''],
       employment: ['', Validators.required],
       employType: ['']
     });
 
-    this.additionalForm.controls['married'].valueChanges.subscribe(value => {
+    this.createDemographicForm.controls['married'].valueChanges.subscribe(value => {
       if (value === "Married") {
         this.showMarrayStatus = true;
       } else {
         this.showMarrayStatus = false;
       }
     });
-    this.additionalForm.controls['employment'].valueChanges.subscribe(value => {
+    this.createDemographicForm.controls['employment'].valueChanges.subscribe(value => {
       if (value === "Employed") {
         this.showEmploymentType = true;
       } else {
@@ -62,53 +77,8 @@ export class CreateDemographicComponent implements OnInit {
 
   }
 
+  onSubmmit(){
+    console.log(this.createDemographicForm.value);
+  }
 }
 
-export const GENDER = [
-  {label: 'Gender', value: null},
-  {label: 'Male', value: 'Male'},
-  {label: 'Female', value: 'Female'}
-];
-
-export const RACE = [
-  {label: 'Race', value: null},
-  {label: 'Native American', value: 'Native American'},
-  {label: 'Asian or Pacific Island', value: ''},
-];
-
-
-export const STATES = [
-  {label: 'States', value: null}
-];
-
-export const LIVING = [
-  {label: 'Alone', value: 'Alone'},
-  {label: 'With Spouse or unrelated partner', value: "With Spouse or unrelated partner"},
-];
-
-export const RESIDENT = [
-  {label: 'One story(no stairs)', value: 'One story(no stairs)'},
-  {label: 'Stairs to home entrance with ramp', value: 'Stairs to home entrance with ramp'}
-];
-
-
-export const MARRYSTATUS = [
-  {label: 'Martial Status', value: null},
-  {label: 'Married', value: 'Married'},
-  {label: 'Single', value: 'Single'},
-  {label: 'With partner', value: 'With partner'},
-];
-
-
-export const EMPLOYMENTS = [
-  {label: 'Employement Status', value: null},
-  {label: 'Employed', value: 'Employed'},
-  {label: 'Unemployed', value: 'Unemployed'},
-];
-
-
-export const EMPLOYSTATUS = [
-  {label: 'Employment Type', value: null},
-  {label: 'Full time', value: 'Full time'},
-  {label: 'Part time', value: 'Part time'},
-]
---
+++ src/app/model/mock.ts
@@ -0,0 +1,107 @@
+export const GENDER = [
+    {label: 'Gender', value: null},
+    {label: 'Male', value: 'Male'},
+    {label: 'Female', value: 'Female'}
+  ];
+  
+  export const RACE = [
+    {label:'Race', value: null},
+    {label:'Caucasian', value: 'Caucasian'},
+    {label:'African American',value:'African American'},
+    {label:'Hispanic',value:'Hispanic'},
+    {label:'Bi-Racial',value:'Bi-Racial'},
+    {label:'Asian',value:'Asian'},
+    {label:'White',value:'White'},
+    {label:'Other',value:'Other'}
+  ];
+  
+  
+  
+  export const LIVING = [
+    {label: 'Alone', value: 'Alone'},
+    {label: 'With Spouse or unrelated partner', value: "With Spouse or unrelated partner"},
+  ];
+  
+  export const RESIDENT = [
+    {label: 'One story(no stairs)', value: 'One story(no stairs)'},
+    {label: 'Stairs to home entrance with ramp', value: 'Stairs to home entrance with ramp'}
+  ];
+  
+  
+  export const MARRYSTATUS = [
+    {label: 'Martial Status', value: null},
+    {label: 'Married', value: 'Married'},
+    {label: 'Single', value: 'Single'},
+    {label: 'With partner', value: 'With partner'},
+  ];
+  
+  
+  export const EMPLOYMENTS = [
+    {label: 'Employement Status', value: null},
+    {label: 'Employed', value: 'Employed'},
+    {label: 'Unemployed', value: 'Unemployed'},
+  ];
+  
+  
+  export const EMPLOYSTATUS = [
+    {label: 'Employment Type', value: null},
+    {label: 'Full time', value: 'Full time'},
+    {label: 'Part time', value: 'Part time'},
+  ];
+
+  export const STATES = [
+    {label: 'States', value: null},
+    {label: 'Alabama', value: 'Alabama'},
+    {label: 'Alaska', value: 'Alaska'},
+    {label: 'Arizona', value: 'Arizona'},
+    {label: 'Arkansas', value: 'Arkansas'},
+    {label: 'California', value: 'California'},
+    {label: 'Colorado', value: 'Colorado'},
+    {label: 'Connecticut', value: 'Connecticut'},
+    {label: 'Delaware', value: 'Delaware'},
+    {label: 'Florida', value: 'Florida'},
+    {label: 'Georgia', value: 'Georgia'},
+    {label: 'Hawaii', value: 'Hawaii'},
+    {label: 'Idaho', value: 'Idaho'},
+    {label: 'Illinios', value: 'Illinios'},
+    {label: 'Indiana', value: 'Indiana'},
+    {label: 'Iowa', value: 'Iowa'},
+    {label: 'Kansas', value: 'Kansas'},
+    {label: 'Kentucky', value: 'Kentucky'},
+    {label: 'Louisiana', value: 'Louisiana'},
+    {label: 'Maine', value: 'Maine'},
+    {label: 'Maryland', value: 'Maryland'},
+    {label: 'Massachusetts', value: 'Massachusetts'},
+    {label: 'Michigan', value: 'Michigan'},
+    {label: 'Minnesota', value: 'Minnesota'},
+    {label: 'Mississippi', value: 'Mississippi'},
+    {label: 'Missouri', value: 'Missouri'},
+    {label: 'Montana', value: 'Montana'},
+    {label: 'Nebraska', value: 'Nebraska'},
+    {label: 'Nevada', value: 'Nevada'},
+    {label: 'New Hampshire', value: 'New Hampshire'},
+    {label: 'New Jersey', value: 'New Jersey'},
+    {label: 'New Mexico', value: 'New Mexico'},
+    {label: 'New York', value: 'New York'},
+    {label: 'North Carolina', value: 'North Carolina'},
+    {label: 'North Dakota', value: 'North Dakota'},
+    {label: 'Ohio', value: 'Ohio'},
+    {label: 'Oklahoma', value: 'Oklahoma'},
+    {label: 'Oregon', value: 'Oregon'},
+    {label: 'Pennsylvania', value: 'Pennsylvania'},
+    {label: 'Rhode Island', value: 'Rhode Island'},
+    {label: 'South Carolina', value: 'South Carolina'},
+    {label: 'South Dakota', value: 'South Dakota'},
+    {label: 'Tennessee', value: 'Tennessee'},
+    {label: 'Texas', value: 'Texas'},
+    {label: 'Utah', value: 'Utah'},
+    {label: 'Vermont', value: 'Vermont'},
+    {label: 'Virginia', value: 'Virginia'},
+    {label: 'Washington', value: 'Washington'},
+    {label: 'West Virgini', value: 'West Virgini'},
+    {label: 'Wisconsin', value: 'Wisconsin'},
+    {label: 'Wyoming', value: 'Wyoming'},
+  ];
+
+
+
